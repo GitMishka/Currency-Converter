0dde890aeea0b58dbf0228f7548f754bf4de7cc8
Misha <mishagrigoryan@gmail.com>
2023-05-13 21:18:18 -0400

    convert

--- app.py
+++ app.py
@@ -13,4 +13,16 @@ def get_rates(base_currency):
         return None
 
 #print(get_rates('USD'))
+def convert_currency(amount, from_currency, to_currency):
+    rates = get_rates(from_currency)
+    if rates:
+        if to_currency in rates:
+            return amount * rates[to_currency]
+        else:
+            print(f"Error: Could not find the currency {to_currency}")
+            return None
+    else:
+        return None
+
+print(convert_currency(100, 'USD', 'EUR'))
 
